c34c2d118a83997953f3cf1390256b77e5fbaa13
Faarkhan <163838798+Faarkhan@users.noreply.github.com>
2024-03-25 07:01:34 +0700

    Add files via upload

---
+++ kasir/function.php
@@ -0,0 +1,36 @@
+<?php
+
+session_start();
+
+//bikin koneksi
+$c = mysqli_connect('localhost','root','','kasir');
+
+//Login
+if (isset($_POST['login'])){
+    //initiate variable
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $check = mysqli_query($c,"SELECT * FROM user WHERE username='$username' and password='$password'");
+    $hitung = mysqli_num_rows($check);
+
+    if($hitung>0){
+        //jika datanya ditemukan
+        //berhasil login
+
+        $_SESSION['login'] = 'True';
+        header('location:index.php');
+
+    } else {
+        //Data tidak ditemukan
+        //gagal login
+        echo '
+        <script>alert("Username atau Password salah");
+        window.location.href="login.php"
+        </script>
+        ';
+    }
+}
+
+
+?>
